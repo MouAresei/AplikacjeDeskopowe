d24d38f1408b23af8b844e4e1f5b0c504f2c1181
adam <ewa.juda@e-zsel.kielce.eu>
2022-09-13 13:55:31 +0200

    utworzenie projektu

---
+++ Aplikacje Deskopowe i mobilne/number01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace number01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine('1');
+            Console.WriteLine("kl6");
+            Console.WriteLine("!");
+        }
+    }
+}
