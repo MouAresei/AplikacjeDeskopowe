f3bb7fdf4a1d6ddab6956c8dd18e340f1eaadcd8
MouAresei <112950401+MouAresei@users.noreply.github.com>
2022-10-26 22:40:28 +0200

    poprawki w kodzie
    poprawki w kodzie

---
+++ werscja odświerzona/klasa-2B-2022-2023-aplikacje-main/Aplikacje desktopowe i mobilne/PolymorphismApp/Dog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismApp
+{
+
+    class Dog : Animal
+    {
+        public Dog() //: base() - to jest domyslne
+        {
+            name = "Azor";
+        }
+
+        public Dog(string name, byte age) : base(name, age)
+        {
+        }
+
+        public Dog(Dog dogToCopy): base(dogToCopy)
+        {
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("***********************");
+            Console.WriteLine("Pies o imieniu " + name + " ma " + age + " lat");
+            Console.WriteLine("***********************");
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("Pies o imieniu " + name + " szczeka");
+        }
+
+        public void ChasePostman()
+        {
+            Console.WriteLine("Pies o imieniu " + name + " goni listonosza");
+        }
+    }
+}
